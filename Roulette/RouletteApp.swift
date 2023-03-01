c6b4a999082e383a901d3d2b201e14759eb36ea0
Pscabby <pscabby0506@gmail.com>
2023-03-01 14:37:50 +0800

    Initial Commit

---
+++ Roulette/RouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouletteApp.swift
+//  Roulette
+//
+//  Created by Abby on 2023/3/1.
+//
+
+import SwiftUI
+
+@main
+struct RouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
